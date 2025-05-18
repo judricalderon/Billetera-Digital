6556b73784e0fd906ff533dbf8198b84495880f1
Miguel Cuellar <mcuellar@opencitylife.com>
2025-05-18 08:40:20 -0500

    MJ ISW1DSAQ/7 Se deja de imprimir logs de seguimiento

--- frontend/src/components/CreateCard.jsx
+++ frontend/src/components/CreateCard.jsx
@@ -55,8 +55,6 @@ export const CreateCard = ({ user }) => {
         idCliente: user.idCliente,
       };
 
-      console.log(cardData)
-
       await addCard(cardData);
 
       setForm({ numero: "", fechaVencimiento: "", cupoTotal: "" });
